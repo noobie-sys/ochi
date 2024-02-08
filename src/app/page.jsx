41aec1f137b7ee6a6ab6057719b497954f54fbe8
Aman Gupta <amangupta954055@gmail.com>
2024-02-08 10:41:08 +0530

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar/Navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between ">
+      <section className="text-[8rem]">
+        <Navbar />
+      </section>
+    </main>
+  );
+}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './navbar.module.scss'
+
+const Navbar = () => {
+  return (
+    <div className={style.navbar}>Navbar</div>
+  )
+}
+
+export default Navbar
